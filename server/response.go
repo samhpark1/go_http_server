cfde591712285121c2bee193eec57e4a21c78d66
samhpark1 <samhpark1@gmail.com>
2025-05-24 17:24:11 -0400

    Response handler/logic

--- server/response.go
+++ server/response.go
@@ -1,1 +1,49 @@
 package server
+
+import (
+	"strconv"
+)
+
+type Response struct {
+	Version string
+	Code    int
+	Reason  string
+	Headers map[string]string
+	Body    []byte
+	Size    int
+}
+
+func CreateResponse(code int, version string, reason string, headers map[string]string, body []byte) *Response {
+	resp := Response{
+		Version: version,
+		Code:    code,
+		Reason:  reason,
+		Headers: headers,
+		Body:    body,
+	}
+
+	if _, ok := headers["Content-Length"]; !ok && len(body) != 0 {
+		headers["Content-Length"] = strconv.Itoa(len(body))
+	}
+
+	return &resp
+}
+
+func (r *Response) ToBytes() []byte {
+	resp_str := ""
+
+	resp_str += r.Version + " " + strconv.Itoa(r.Code) + " " + r.Reason + "\r\n"
+
+	for k, v := range r.Headers {
+		resp_str += k + ": " + v + "\r\n"
+	}
+
+	if len(r.Body) != 0 {
+		resp_str += "\r\n"
+	}
+
+	resp_str += string(r.Body)
+
+	return []byte(resp_str)
+
+}
